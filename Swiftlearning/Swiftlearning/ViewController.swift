2c75e41669e91146f07c5f27cdc96baac3c07f10
sramika mangalapurapu <sramikamangalapurapu@sramikas-MacBook-Pro.local>
2020-03-02 10:18:45 +0530

    swift project created

---
+++ Swiftlearning/Swiftlearning/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Swiftlearning
+//
+//  Created by sramika mangalapurapu on 3/2/20.
+//  Copyright © 2020 sramika mangalapurapu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
